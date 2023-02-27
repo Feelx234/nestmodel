a2c1ec79e1e020edfcb277045e1929241a87910c
Felix Ihno Stamm <felix.stamm@rwth-aachen.de>
2023-02-27 14:26:55 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='nestmodel',
     version='0.1.1',
@@ -7,6 +10,8 @@ setup(
     author='Felix Stamm',
     author_email='felix.stamm@cssh.rwth-aachen.de',
     description='This package contains the implementation of the neighborhood structure configuration model for python',
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
     install_requires=[
               'pandas', 'scipy', 'numpy', 'matplotlib', 'numba', 'networkx', 'brokenaxes', 'nbconvert', 'tqdm'
           ],
